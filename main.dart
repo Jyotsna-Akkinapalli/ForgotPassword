274ab5ebe245c42826a70047f413a61ef935481f
Jyotsna-Akkinapalli <40540136+Jyotsna-Akkinapalli@users.noreply.github.com>
2023-12-24 10:03:29 +0530

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,35 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:lab9/login.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: _initialization,
+        builder: (context, snapshot) {
+          // Check for Errors
+          if (snapshot.hasError) {
+            print("Something Went Wrong");
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+          return MaterialApp(
+            title: 'Flutter Firebase EMail Password Auth',
+            theme: ThemeData(
+              primarySwatch: Colors.deepPurple,
+            ),
+            debugShowCheckedModeBanner: false,
+            home: Login(),
+          );
+        });
+  }
+}
